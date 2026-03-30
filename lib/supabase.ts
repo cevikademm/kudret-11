85ad13113bc26919435ef7e231d49d4afb7bae3d
cevikademm <cevikademm@github.com>
2026-03-30 19:10:12 +0300

    update: switch to new supabase project

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,9 +2,9 @@
 import { createClient } from '@supabase/supabase-js';
 
 // GÜNCELLEME: Proje URL'niz
-const supabaseUrl = 'https://hlyxaqdimvwcrlssxczu.supabase.co';
+const supabaseUrl = 'https://frbcbhpqgywosmbklxhc.supabase.co';
 
 // GÜNCELLEME: Anon Public Key
-const supabaseAnonKey = 'sb_publishable_lt-Ysij50RJE0cX_eYURnQ_NZwCvuan'; 
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZyYmNiaHBxZ3l3b3NtYmtseGhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ4ODY3MTgsImV4cCI6MjA5MDQ2MjcxOH0.3MKIJR0QygC6vKdOJxuPv8yymxQ4TAWg2Sm0dB7vR4w';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
